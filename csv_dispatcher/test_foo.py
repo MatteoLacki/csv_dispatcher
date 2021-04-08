34e966f03c4cc5f711572cb3472e960c6211df22
DanaHein <danahein@uni-mainz.de>
2021-04-08 15:24:10 +0200

    working diann csv_dispatcher

--- csv_dispatcher/test_foo.py
+++ csv_dispatcher/test_foo.py
@@ -1,15 +1,21 @@
 import numpy as np
+from subprocess import run
 from limspy.dia_nn import dia_nn
 
-def foo(dicto):
-	#Experiment,Fasta,Outdir,Spectrallib,Exe,FDR,Cut,Missedcl,Pepmin,Pepmax,Precmin,Precmax,Fragmin,Fragmax,Massaccuracy,MS1accuracy,massaccuracycal,ProteinGroup,Quantification,Normalisation,Librarygen,NN,mbr,individualruns,Threads
-    #dia_nn(experiments=Experiment,fastas=Fasta,threads=Threads,fdr=FDR,min_pep_len=Pepmin,max_pep_len=Pepmax,max_fr_mz=Fragmax,min_fr_mz=Fragmin, min_pr_mz=Precmin,max_pr_mz=Precmax, normalisation=Normalisation,miss_cleavages=Missedcl,cut=Cut,quantification=Quantification, exe_path=Exe)
-    cmd= dia_nn(**dicto)
-    return cmd
+def foo(x,y,z):
+    x = np.array(x)
+    y = np.array(y)
+    z = np.array(z)
+    return x,y,z
+
+def diann_foo(**args):
+	cmd, cmdpdf = dia_nn(**args)
+	run(cmd)
+	run(cmdpdf)
 
 def print_foo(x,y,z):
     return print(foo(x,y,z))
 
 def foo2(number, string):
     print(number + number)
-    print(string)add
+    print(string)
---
+++ untitled0.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 30 17:04:59 2021
+
+@author: Dana
+"""
+import bin.csv_dispatch
+
+
