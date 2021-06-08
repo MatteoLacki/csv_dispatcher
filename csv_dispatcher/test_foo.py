1b12dd46564ecf71da488c16ad1099fc3c8345b7
DanaHein <danahein@uni-mainz.de>
2021-06-08 10:19:32 +0200

    added MQ function for csv_dispatcher

--- csv_dispatcher/test_foo.py
+++ csv_dispatcher/test_foo.py
@@ -1,6 +1,7 @@
 import numpy as np
 from subprocess import run
 from limspy.dia_nn import dia_nn
+from limspy.parse_mqpar import gen_mqpar
 
 def foo(x,y,z):
     x = np.array(x)
@@ -9,9 +10,14 @@ def foo(x,y,z):
     return x,y,z
 
 def diann_foo(**args):
-	cmd, cmdpdf = dia_nn(**args)
-	run(cmd)
-	run(cmdpdf)
+    cmd, cmdpdf = dia_nn(**args)
+    run(cmd)
+    run(cmdpdf)
+
+def maxquant_foo(**args):
+    gen_mqpar(**args)
+    cmd= rf"powershell.exe dotnet {args['exe_path']} {args['output_path']}/{args['exp_name']}.xml"
+    run(cmd)
 
 def print_foo(x,y,z):
     return print(foo(x,y,z))
